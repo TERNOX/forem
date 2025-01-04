d0b179071eee05f40e863ae1f5103c9ca4324b60
TERNOX <ternox1@gmail.com>
2025-01-04 23:46:56 +0200

    update to new version

--- app/javascript/packs/baseInitializers.js
+++
@@ -1,5 +0,0 @@
-import { initializeCommentDate } from './initializers/initializeCommentDate';
-import { initializeCommentPreview } from './initializers/initializeCommentPreview';
-
-initializeCommentDate();
-initializeCommentPreview();
--- uffizzi/db_seed.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-bundle exec rake db:seed
-
-tail -f /dev/null
